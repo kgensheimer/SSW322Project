0ef3ab1981d44d1ad894b987539733ab3acd5c3c
Kyle Gensheimer <kylegensheimer@gmail.com>
2019-03-27 12:24:23 -0400

    Connected to Firebase
    Also fixed version issues

--- app/src/main/java/com/example/ssw_322_project/MainActivity.java
+++ app/src/main/java/com/example/ssw_322_project/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ssw_322_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button1 = findViewById(R.id.button1);
     }
+
+
 }
