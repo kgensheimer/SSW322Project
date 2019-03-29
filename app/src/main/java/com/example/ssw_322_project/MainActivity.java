81f0bceb34902df37b4c9c22951c578ef819eb44
Kyle Gensheimer <kylegensheimer@gmail.com>
2019-03-28 21:48:43 -0400

    Login & Signup XML Pages Complete
    No functionality added yet

--- app/src/main/java/com/example/ssw_322_project/MainActivity.java
+++ app/src/main/java/com/example/ssw_322_project/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.ssw_322_project;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.*;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button1 = findViewById(R.id.button1);
     }
-
 
 }
