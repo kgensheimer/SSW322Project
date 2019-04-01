220fd3b938a0aaecb7083186e5ecbda5c0fcb255
Eamon <eamonbontempo@aol.com>
2019-04-01 16:10:34 -0400

    Create Test.java

---
+++ app/src/main/java/com/example/ssw_322_project/ClassesAndInterfaces/Test.java
@@ -0,0 +1,28 @@
+package com.example.ssw_322_project.ClassesAndInterfaces;
+
+import java.util.ArrayList;
+
+import Form;
+
+public class Test implements Form {
+    private String name;
+    private int ID;
+    private ArrayList<Question> questionArrayList = new ArrayList<Question>();
+
+
+
+    @Override
+    public void addQuestion() {
+
+    }
+
+    @Override
+    public void deleteQuestion() {
+
+    }
+
+    @Override
+    public void tabulateResults() {
+
+    }
+}
